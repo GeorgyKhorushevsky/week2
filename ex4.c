e723834a97dcf227171a9d97a9be09e0d667a518
GeorgyKhorushevsky <35738786+GeorgyKhorushevsky@users.noreply.github.com>
2018-08-29 22:59:37 +0300

    Update ex4.c

--- ex4.c
+++ ex4.c
@@ -2,15 +2,15 @@
 
 
 void swap(int* x, int* y){
-    *x=*x^*y;
-    *y=*x^*y;
-    *x=*x^*y;
+    *y=(*x) ^ (*y);
+    *x=(*x) ^ (*y);
+    *y=(*x) ^ (*y);
 }
 
 int main(){
     int x, y;
-    scanf("x: %d \n", x);
-    scanf("y: %d \n", y);
+    scanf("x: %d \n", &x);
+    scanf("y: %d \n", &y);
     swap(&x, &y);
     printf("new x: %d \n", x);
     printf("new y: %d \n", y);
