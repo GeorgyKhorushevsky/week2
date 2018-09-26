8155cbd8206df639dd61b9f0313f9388bc7c3389
GeorgyKhorushevsky <35738786+GeorgyKhorushevsky@users.noreply.github.com>
2018-09-26 22:48:53 +0300

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#define N "Nikita is the best TA ever"
+
+
+int main () {
+    char *s = N;
+    int buffsize = strlen(s)+1;
+    char *transfer = malloc(buffsize);
+    int pipefd[2];
+    pipe(pipefd);
+    write(pipefd[1], s, buffsize);
+    printf("Sended: %s\n", s);
+    read(pipefd[0], transfer, buffsize);
+    printf("Received: %s\n", transfer);
+    return 0;
+}
